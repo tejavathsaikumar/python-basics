f4ac1f2d93c00722975a79e0d66837ebc0e1d81d
Saikumar Tejavath <tejavathsaikumar70@gmail.com>
2026-02-01 14:22:25 -0500

    Add loop examples in pythoon

---
+++ loops.py
@@ -0,0 +1,13 @@
+# loops.py
+
+# For loop example
+for i in range(1, 6):
+    print("Number:", i)
+
+print("-----")
+
+# While loop example
+count = 1
+while count <= 5:
+    print("Count:", count)
+    count += 1
